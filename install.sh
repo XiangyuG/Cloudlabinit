cb93beae1925c52cb4f0961c3925aa846c0e9445
Xiangyu Gao <xiangyug@cs.washington.edu>
2025-12-14 10:37:18 -0700

    Add installation script

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo apt update
+
+sudo apt install -y zip bison build-essential cmake flex git libedit-dev libllvm12 llvm-12-dev libclang-12-dev python zlib1g-dev libelf-dev libfl-dev python3-setuptools liblzma-dev arping iperf
