0386e43b3547cafe65715fa161751941f120d161
xiang95 <xiang95@localhost>
2025-12-14 14:08:55 -0700

    Update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 sudo apt update
 
 sudo apt install -y zip bison build-essential cmake flex git libedit-dev libllvm12 llvm-12-dev libclang-12-dev python zlib1g-dev libelf-dev libfl-dev python3-setuptools liblzma-dev arping iperf
+
+sudo apt install -y docker.io
+
+sudo apt install -y docker-buildx
+
+sudo apt install -y git build-essential libssl-dev
+
+sudo apt install htop
